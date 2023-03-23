cd431e6d7339ea16c355fce905c9e1509a48cce6
JarcauCristian <stefan.jarcau@student.unitbv.ro>
2023-03-23 09:24:51 +0200

    Added some test

--- src/test/java/com/example/uitesting/MainPageTest.java
+++ src/test/java/com/example/uitesting/MainPageTest.java
@@ -3,9 +3,14 @@ package com.example.uitesting;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+
+import java.util.Date;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.WebDriverRunner.url;
@@ -124,6 +129,39 @@ public class MainPageTest {
         {
             $("html > body > app-layout > div > div > div > div > div > app-content > app-builder > div > mat-table > mat-row:nth-of-type(" + String.valueOf(i) + ") > mat-cell:nth-of-type(3)").shouldHave(text("Accepted"));
         }
+    }
+
+    @Test
+    public void addPatientFormWorks() {
+        String setDateScript = "arguments[0].value = arguments[1]";
+
+        $("input[id='username']").setValue("dev");
+        $("input[id='password']").setValue("qwer1234");
+        $("input[id='kc-login']").click();
+        sleep(1000);
+        $("a[id*='toolbar']").click();
+        sleep(1000);
+        $("input[id='firstName']").setValue("John");
+        $("input[id='lastName']").setValue("Harris");
+        SelenideElement inputElement = $("input[type='date']");
+        JavascriptExecutor jsExecutor = (JavascriptExecutor) WebDriverRunner.getWebDriver();
+        jsExecutor.executeScript(setDateScript, inputElement, "02-Jul-2000");
+        $("input[id='address']").setValue("Elden Street 3A");
+        $("input[id='zip']").setValue("400303");
+        $("#kt_content_container > app-builder > div > div.card-body > div > div > form > div:nth-child(2) > div:nth-child(3) > mat-form-field").click();
+        $("mat-option[id='mat-option-01'] span[class='mat-option-text']").sendKeys(Keys.ENTER);
+        $("input[id='telephone']").setValue("0775678970");
+        $("input[id='mat-input-7']").setValue("john.harris@gmail.com");
+        inputElement = $("input[type='date']");
+        jsExecutor = (JavascriptExecutor) WebDriverRunner.getWebDriver();
+        jsExecutor.executeScript(setDateScript, inputElement, "23-Mar-2023");
+        $("div[id='mat-select-value-5']").setValue("ccmname1");
+        $("input[id='mat-input-9']").setValue("caregiver1@gmail.com");
+
+        $("button[class='btn btn-lg btn-primary me-3']").click();
+        sleep(1000);
+
+        assertEquals("https://retention-csb-test.biomed.ntua.gr/patients-monitoring", url());
     }
 
 }
